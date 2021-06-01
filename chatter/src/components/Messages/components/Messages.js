c4f3bc30401d06903e24aacbc56a060fbec6602b
Trannum Sharma <trannum@apptunix.com>
2021-06-01 21:06:29 +0530

    Written functions and states for message and array of messages

--- chatter/src/components/Messages/components/Messages.js
+++ chatter/src/components/Messages/components/Messages.js
@@ -1,30 +1,54 @@
-import React, { useContext } from 'react';
-import io from 'socket.io-client';
-import useSound from 'use-sound';
-import config from '../../../config';
-import LatestMessagesContext from '../../../contexts/LatestMessages/LatestMessages';
-import TypingMessage from './TypingMessage';
-import Header from './Header';
-import Footer from './Footer';
-import Message from './Message';
-import '../styles/_messages.scss';
+import React, { useContext } from "react";
+import io from "socket.io-client";
+import useSound from "use-sound";
+import config from "../../../config";
+import LatestMessagesContext from "../../../contexts/LatestMessages/LatestMessages";
+import TypingMessage from "./TypingMessage";
+import Header from "./Header";
+import Footer from "./Footer";
+import Message from "./Message";
+import "../styles/_messages.scss";
+import { useState } from "react";
+import INIT_BOT_MSG from "../../../common/constants/initialBottyMessage";
 
-const socket = io(
-  config.BOT_SERVER_ENDPOINT,
-  { transports: ['websocket', 'polling', 'flashsocket'] }
-);
+const socket = io(config.BOT_SERVER_ENDPOINT, {
+  transports: ["websocket", "polling", "flashsocket"],
+});
 
 function Messages() {
   const [playSend] = useSound(config.SEND_AUDIO_URL);
   const [playReceive] = useSound(config.RECEIVE_AUDIO_URL);
   const { setLatestMessage } = useContext(LatestMessagesContext);
 
+  //Creating a state to hold messages
+  const [message, setMessage] = useState("");
+
+  //Defining onChangeMessage
+  const onChangeMessage = (event) => {
+    setMessage(event.target.value);
+  };
+
+  //Creating a messages array for message list
+  const [messages, setMessages] = useState([INIT_BOT_MSG]);
+
+  //Defining sendMessage
+
+  const sendMessage = () => {
+    setMessages([...messages, message]);
+  };
+
+  //Creating a boolean state to check if bot is typing or not
+  const [isBotTyping, setIsBotTyping] = useState(false);
+
   return (
     <div className="messages">
       <Header />
-      <div className="messages__list" id="message-list">
-      </div>
-      <Footer message={message} sendMessage={sendMessage} onChangeMessage={onChangeMessage} />
+      <div className="messages__list" id="message-list"></div>
+      <Footer
+        message={message}
+        sendMessage={sendMessage}
+        onChangeMessage={onChangeMessage}
+      />
     </div>
   );
 }
